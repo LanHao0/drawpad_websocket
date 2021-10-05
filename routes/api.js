fc356cbd6f873771146f9379b62877c6a02cf75c
LanHao <a913538526@gmail.com>
2021-10-05 19:32:14 +0800

    api增加颜色阴影等支持 增加描述文件

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ const WebSocket = require('ws');
 
 const server = new WebSocket.Server({
     clientTracking: true,
-    port: 8080
+    port: 8083
 });
 
 let drawHistory;
@@ -19,12 +19,29 @@ server.on('connection', function connection(ws) {
     console.log('server: receive connection.');
 
     ws.on('message', function incoming(message) {
-        console.log(message)
-        drawHistory = message;
+        let data = JSON.parse(message);
+
+        let split = data.split_character;
+        data.clickX.push(data.clickX[data.clickX.length - 1]);
+        data.clickY.push(data.clickY[data.clickY.length - 1]);
+        data.brushSize.push(data.brushSize[data.brushSize.length - 1]);
+        data.brushColor.push(data.brushColor[data.brushColor.length - 1]);
+        data.dragging.push(1);
+        data.end_draw.push(null);
+
+
+        data = data.clickX + split +
+            data.clickY + split +
+            data.brushSize + split +
+            data.brushColor + split +
+            data.dragging + split +
+            data.end_draw;
+
+        drawHistory = data;
 
         server.clients.forEach(function (client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message, function (err) {
+                client.send(data, function (err) {
                     err && console.log(err);
                 });
             }
